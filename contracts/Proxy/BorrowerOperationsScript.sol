37408d2b3b22f8008bfbe9e7ba1870e3c522c59c
FoXy <12250394+theonlyfoxy@users.noreply.github.com>
2021-04-03 17:58:16 +0430

    Transfer back the debt to the Proxy's owner

--- contracts/Proxy/BorrowerOperationsScript.sol
+++ contracts/Proxy/BorrowerOperationsScript.sol
@@ -2,44 +2,88 @@
 
 pragma solidity 0.6.11;
 
-import "../Dependencies/CheckContract.sol";
-import "../Interfaces/IBorrowerOperations.sol";
+import "./Interfaces/IBorrowerOperations.sol";
+import "./Interfaces/IERC20.sol";
 
-
-contract BorrowerOperationsScript is CheckContract {
+contract BorrowerOperationsScript {
     IBorrowerOperations immutable borrowerOperations;
+    address LUSD;
 
-    constructor(IBorrowerOperations _borrowerOperations) public {
-        checkContract(address(_borrowerOperations));
+    constructor(IBorrowerOperations _borrowerOperations, address _LUSD) public {
         borrowerOperations = _borrowerOperations;
+        LUSD = _LUSD;
     }
 
-    function openTrove(uint _maxFee, uint _LUSDAmount, address _upperHint, address _lowerHint) external payable {
+    function openTrove(
+        uint256 _maxFee,
+        uint256 _LUSDAmount,
+        address _upperHint,
+        address _lowerHint
+    ) external payable {
         borrowerOperations.openTrove{ value: msg.value }(_maxFee, _LUSDAmount, _upperHint, _lowerHint);
+
+        IERC20(LUSD).transfer(msg.sender, _LUSDAmount);
     }
 
     function addColl(address _upperHint, address _lowerHint) external payable {
         borrowerOperations.addColl{ value: msg.value }(_upperHint, _lowerHint);
     }
 
-    function withdrawColl(uint _amount, address _upperHint, address _lowerHint) external {
+    function withdrawColl(
+        uint256 _amount,
+        address _upperHint,
+        address _lowerHint
+    ) external {
         borrowerOperations.withdrawColl(_amount, _upperHint, _lowerHint);
     }
 
-    function withdrawLUSD(uint _maxFee, uint _amount, address _upperHint, address _lowerHint) external {
+    function withdrawLUSD(
+        uint256 _maxFee,
+        uint256 _amount,
+        address _upperHint,
+        address _lowerHint
+    ) external {
         borrowerOperations.withdrawLUSD(_maxFee, _amount, _upperHint, _lowerHint);
     }
 
-    function repayLUSD(uint _amount, address _upperHint, address _lowerHint) external {
+    function repayLUSD(
+        uint256 _amount,
+        address _upperHint,
+        address _lowerHint
+    ) external {
         borrowerOperations.repayLUSD(_amount, _upperHint, _lowerHint);
     }
 
-    function closeTrove() external {
+    function closeTrove(
+        uint256 _debtAmount,
+        uint256 _collAmount,
+        address payable _collreceiver
+    ) external {
+        IERC20(LUSD).approve(address(borrowerOperations), _debtAmount);
+
         borrowerOperations.closeTrove();
+
+        if (_collreceiver != address(this)) _collreceiver.transfer(_collAmount);
     }
 
-    function adjustTrove(uint _maxFee, uint _collWithdrawal, uint _debtChange, bool isDebtIncrease, address _upperHint, address _lowerHint) external payable {
-        borrowerOperations.adjustTrove{ value: msg.value }(_maxFee, _collWithdrawal, _debtChange, isDebtIncrease, _upperHint, _lowerHint);
+    function adjustTrove(
+        uint256 _maxFee,
+        uint256 _collWithdrawal,
+        uint256 _debtChange,
+        bool isDebtIncrease,
+        address _upperHint,
+        address _lowerHint
+    ) external payable {
+        borrowerOperations.adjustTrove{ value: msg.value }(
+            _maxFee,
+            _collWithdrawal,
+            _debtChange,
+            isDebtIncrease,
+            _upperHint,
+            _lowerHint
+        );
+
+        if (isDebtIncrease) IERC20(LUSD).transfer(msg.sender, _debtChange);
     }
 
     function claimCollateral() external {
